58d3dc1e3c6c69e9414a8e2db722f191486e6f35
Naren404 <narendrasunar47@gmail.com>
2022-01-09 22:45:46 +1100

    Rails Code Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/orders', to: 'orders#index'
+  post '/update_status/:id', to: 'orders#update_status'
   root 'orders#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
